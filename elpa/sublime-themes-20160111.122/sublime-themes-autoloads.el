a4d0548514c2bc48fc7c471d8a931d305e9be61d
Robert Zalavary <Robert.Zalavary@frontiersmart.com>
2017-01-24 17:49:42 +0000

    emacs init

---
+++ elpa/sublime-themes-20160111.122/sublime-themes-autoloads.el
@@ -0,0 +1,120 @@
+;;; sublime-themes-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "brin-theme" "brin-theme.el" (22525 63975 484599
+;;;;;;  745000))
+;;; Generated autoloads from brin-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "dorsey-theme" "dorsey-theme.el" (22525 63975
+;;;;;;  520613 736000))
+;;; Generated autoloads from dorsey-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))) (when (not window-system) (custom-set-faces '(default ((t (:background nil)))))))
+
+;;;***
+
+;;;### (autoloads nil "fogus-theme" "fogus-theme.el" (22525 63975
+;;;;;;  461788 606000))
+;;; Generated autoloads from fogus-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))) (when (not window-system) (custom-set-faces '(default ((t (:background nil)))))))
+
+;;;***
+
+;;;### (autoloads nil "graham-theme" "graham-theme.el" (22525 63975
+;;;;;;  536621 850000))
+;;; Generated autoloads from graham-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "granger-theme" "granger-theme.el" (22525 63975
+;;;;;;  561726 619000))
+;;; Generated autoloads from granger-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "hickey-theme" "hickey-theme.el" (22525 63975
+;;;;;;  582787 195000))
+;;; Generated autoloads from hickey-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))) (when (not window-system) (custom-set-faces '(default ((t (:background nil)))))))
+
+;;;***
+
+;;;### (autoloads nil "junio-theme" "junio-theme.el" (22525 63975
+;;;;;;  406788 34000))
+;;; Generated autoloads from junio-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))) (when (not window-system) (custom-set-faces '(default ((t (:background nil)))))))
+
+;;;***
+
+;;;### (autoloads nil "mccarthy-theme" "mccarthy-theme.el" (22525
+;;;;;;  63975 487551 736000))
+;;; Generated autoloads from mccarthy-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "odersky-theme" "odersky-theme.el" (22525 63975
+;;;;;;  442537 226000))
+;;; Generated autoloads from odersky-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "ritchie-theme" "ritchie-theme.el" (22525 63975
+;;;;;;  446571 536000))
+;;; Generated autoloads from ritchie-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "spolsky-theme" "spolsky-theme.el" (22525 63975
+;;;;;;  579609 18000))
+;;; Generated autoloads from spolsky-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "sublime-themes" "sublime-themes.el" (22525
+;;;;;;  63975 553532 636000))
+;;; Generated autoloads from sublime-themes.el
+
+(when (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "wilson-theme" "wilson-theme.el" (22525 63975
+;;;;;;  478613 112000))
+;;; Generated autoloads from wilson-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))) (when (not window-system) (custom-set-faces '(default ((t (:background nil)))))))
+
+;;;***
+
+;;;### (autoloads nil nil ("sublime-themes-pkg.el") (22525 63975
+;;;;;;  516788 521000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sublime-themes-autoloads.el ends here
